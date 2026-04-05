8afaab501540c89589764de39e5d91e80bbf0123
sizxiwu <121373708+sizxiwu@users.noreply.github.com>
2026-04-06 03:08:10 +0800

    Update __init__.py

--- core/luckmail/__init__.py
+++ core/luckmail/__init__.py
@@ -1,4 +1,71 @@
-from .email_loader import PurchasedEmailLoader, PurchasedEmail, EmailStatus
+"""
+LuckMailSdk - Python SDK for LuckMail Email System
+支持同步/异步双模式，智能识别调用上下文自动切换
+"""
 
-# Existing exports
-# ... existing code here ...
+from .client import LuckMailClient
+from .user import UserAPI
+from .supplier import SupplierAPI
+from .exceptions import (
+    LuckMailError,
+    AuthError,
+    APIError,
+    NetworkError,
+    TimeoutError,
+)
+from .models import (
+    UserInfo,
+    EmailItem,
+    ProjectItem,
+    OrderInfo,
+    OrderCode,
+    PurchaseItem,
+    TagItem,
+    TokenCode,
+    TokenAliveResult,
+    TokenMailItem,
+    TokenMailList,
+    TokenMailDetail,
+    AppealInfo,
+    SupplierProfile,
+    SupplierEmailItem,
+    AppealItem,
+    DashboardSummary,
+)
+from .email_loader import (
+    PurchasedEmailLoader,
+    PurchasedEmail,
+    EmailStatus,
+)
+
+__version__ = "1.2.1"
+__all__ = [
+    "LuckMailClient",
+    "UserAPI",
+    "SupplierAPI",
+    "LuckMailError",
+    "AuthError",
+    "APIError",
+    "NetworkError",
+    "TimeoutError",
+    "UserInfo",
+    "EmailItem",
+    "ProjectItem",
+    "OrderInfo",
+    "OrderCode",
+    "PurchaseItem",
+    "TagItem",
+    "TokenCode",
+    "TokenAliveResult",
+    "TokenMailItem",
+    "TokenMailList",
+    "TokenMailDetail",
+    "AppealInfo",
+    "SupplierProfile",
+    "SupplierEmailItem",
+    "AppealItem",
+    "DashboardSummary",
+    "PurchasedEmailLoader",
+    "PurchasedEmail",
+    "EmailStatus",
+]
